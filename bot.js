b2c1b38c97891bcc67c8abe6c688e458e1e36348
Leomotors <59821765+Leomotors@users.noreply.github.com>
2021-07-01 23:27:30 +0700

    now able to shoot questions

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@
 
 const auth = require("./auth.json")
 const logconsole = require("./utils/logconsole")
+const { exec } = require("child_process")
 
 const Discord = require("discord.js")
 
@@ -15,6 +16,56 @@ client.on("ready", () => {
 })
 
 client.login(auth.token)
+
+client.on("message", eval_msg)
+
+class Gaym {
+    constructor() {
+        this.questions_dict = undefined
+        this.questions = undefined
+        this.count = -1
+        this.index = 0
+        this.ready = false
+    }
+
+    init(channel) {
+        exec(`./polygen 20 3 2 10 noice __JSON_MODE__`, (error, stdout, stderr) => {
+            this.questions_dict = JSON.parse(stdout)
+            this.questions = Object.keys(this.questions_dict.questions)
+            this.count = this.questions.length
+            this.ready = true
+            this.ask(channel)
+        })
+    }
+
+    ask(channel) {
+        channel.send(this.questions[this.index])
+        this.index++
+    }
+}
+
+let current_gaym = undefined
+function eval_msg(msg) {
+    if (msg.author.id == client.user.id) {
+        // * It's your own message!
+        return
+    }
+
+    logconsole(`Recieve message from ${msg.author.tag} : ${msg.content}`)
+
+    if (msg.content.startsWith("!challenge")) {
+        msg.channel.send(`<@!${msg.author.id}> you dare challenge me?`)
+        current_gaym = new Gaym()
+        current_gaym.init(msg.channel)
+        return
+    }
+
+    if (current_gaym)
+    {
+        current_gaym.ask(msg.channel)
+    }
+}
+
 
 // * DEBUG ZONE
 
